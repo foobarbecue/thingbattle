c339425674857fd2031e19eb69ba086edfa5676a
thatneat <thatneat@gmail.com>
2017-11-23 20:09:37 -0800

    delete commented code

--- client.js
+++ client.js
@@ -33,35 +33,6 @@ var Client = IgeClass.extend({
 
             }
         });
-
-        /*
-        // Load a game texture here
-        this.gameTextures.balloon = new IgeTexture('./assets/balloon.png');
-        // Wait for our textures to load before continuing
-        ige.on('texturesLoaded', function () {
-            // Create the HTML canvas
-            ige.createFrontBuffer(true);
-
-            // Start the engine
-            ige.start(function (success) {
-                // Check if the engine started successfully
-                if (success) {
-                    // Add base scene data
-                    ige.addGraph('IgeBaseScene');
-
-                    const baseScene = ige.$('baseScene');
-                    console.log('adding balloon');
-                    newBalloon = new IgeUiEntity();
-                    newBalloon.texture(self.gameTextures.balloon)
-                        .addComponent(IgeVelocityComponent)
-                        .scaleTo(1.5,1.5,1.5)
-                        .velocity.y(-0.1);
-                    self.balloons.push(newBalloon);
-                    newBalloon.mount(baseScene);
-                }
-            });
-        });
-        */
 	},
 });
 
